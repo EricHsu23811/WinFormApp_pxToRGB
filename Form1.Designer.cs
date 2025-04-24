1e91fd3d3a71bfe7e465bf18048ef3970467703a
EricHsu23811 <eric1491@gmail.com>
2025-04-24 17:05:20 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WinFormApp_pxToRGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImgSelect = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnToBGR = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblSWver = new System.Windows.Forms.Label();
+            this.txtMac = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnImgSelect
+            // 
+            this.btnImgSelect.Location = new System.Drawing.Point(12, 44);
+            this.btnImgSelect.Name = "btnImgSelect";
+            this.btnImgSelect.Size = new System.Drawing.Size(138, 40);
+            this.btnImgSelect.TabIndex = 0;
+            this.btnImgSelect.Text = "SelectImage";
+            this.btnImgSelect.UseVisualStyleBackColor = true;
+            this.btnImgSelect.Click += new System.EventHandler(this.btnImgSelect_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFilePath.Location = new System.Drawing.Point(12, 89);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(776, 25);
+            this.txtFilePath.TabIndex = 7;
+            // 
+            // btnToBGR
+            // 
+            this.btnToBGR.Location = new System.Drawing.Point(650, 119);
+            this.btnToBGR.Name = "btnToBGR";
+            this.btnToBGR.Size = new System.Drawing.Size(138, 40);
+            this.btnToBGR.TabIndex = 8;
+            this.btnToBGR.Text = "ToBGR image";
+            this.btnToBGR.UseVisualStyleBackColor = true;
+            this.btnToBGR.Click += new System.EventHandler(this.btnToBGR_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 343);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 25);
+            this.statusStrip1.TabIndex = 93;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 19);
+            this.toolStripStatusLabel1.Text = "Status text";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSWver
+            // 
+            this.lblSWver.AutoSize = true;
+            this.lblSWver.Location = new System.Drawing.Point(12, 317);
+            this.lblSWver.Name = "lblSWver";
+            this.lblSWver.Size = new System.Drawing.Size(83, 15);
+            this.lblSWver.TabIndex = 94;
+            this.lblSWver.Text = "SW_ver_date";
+            // 
+            // txtMac
+            // 
+            this.txtMac.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMac.Enabled = false;
+            this.txtMac.Font = new System.Drawing.Font("新細明體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtMac.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtMac.Location = new System.Drawing.Point(600, 44);
+            this.txtMac.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMac.Name = "txtMac";
+            this.txtMac.Size = new System.Drawing.Size(188, 21);
+            this.txtMac.TabIndex = 96;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.Location = new System.Drawing.Point(542, 46);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 19);
+            this.label13.TabIndex = 97;
+            this.label13.Text = "MAC :";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 98;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Enabled = false;
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(73, 23);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 23);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(134, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 368);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtMac);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblSWver);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnToBGR);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnImgSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImgSelect;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnToBGR;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label lblSWver;
+        private System.Windows.Forms.TextBox txtMac;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;   //
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp_pxToRGB
+{
+    /* ---------------------------------------------------------
+     * WINAPI STUFF
+     * ------------------------------------------------------ */
+    public enum pxColor : Byte 
+    {
+        Red = 0,
+        Green = 1,
+        Blue = 2,
+    }
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*******************************************************************************
+         * Constant 
+         ******************************************************************************/
+        /* These are constant variables */
+        public static readonly string userDocFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        
+        /*******************************************************************************
+         * Gobal Variable 
+         ******************************************************************************/
+        static string userAppFolder = userDocFolder + "\\pxToRGB";
+        // 可選：獲取完整路徑
+        string executablePath = Assembly.GetExecutingAssembly().Location;
+        // 獲取目前執行檔的名稱
+        static string fileName = Assembly.GetExecutingAssembly().GetName().Name;
+        //獲取執行檔版本
+        string fileVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        string buildDateTime = System.IO.File.GetCreationTime(Assembly.GetExecutingAssembly().Location).ToString("yyyy-MM-dd HH:mm:ss");
+
+        string strFilePath = "";    //userAppFolder + "\\" + "H4_IQC_LBS_BURN.csv";
+        static string strDailyLogDate = DateTime.Now.ToString("yyyy-MM-dd");
+        static string strFileDailyFolder = userAppFolder + "\\" + DateTime.Now.ToString("yyyy")
+            + "\\" + DateTime.Now.ToString("MM") + "\\" + DateTime.Now.ToString("dd");
+        static string strLogNameDaily = ""; //strFileDailyFolder + "\\" + "H4_IQC_LBS_BURN" + "_" + strDailyLogDate + ".csv";
+        string strFileDailyPath = "";   //userAppFolder + "\\" + strLogNameDaily;    //@"\Burn1to10.csv";
+        string strSwVer = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion.ToString();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtFilePath.ReadOnly = true;
+            txtFilePath.BackColor = SystemColors.Info;
+            toolStripStatusLabel1.Text = "Select a image file to convert.";
+            btnToBGR.Enabled = false;
+
+            txtMac.Text = GetMacAddress().ToString();
+            lblSWver.Text = "SW : " + fileName + " - ver: " + strSwVer + " ; 程式碼日期 : " + buildDateTime;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            utilHelpAbout();
+        }
+        private void utilHelpAbout()  
+        {
+            string company = "Megaforce(R) " + fileName + " Ver:" + fileVersion + "\n";
+            string copyright = "(C)Copyright Megaforce Corp 2023-2033.\n";
+            string buildDT = "Built on " + buildDateTime + "\n";
+            string Author = "Author: Eric Hsu\n";
+            MessageBox.Show(company + copyright + buildDT + Author, "About " + fileName);
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)  
+        {
+            this.Close();
+        }
+        public static string GetMacAddress()
+        {
+            string macAddresses = "";
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)  //2023-03-21
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+                else if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            return macAddresses;
+        }
+        private void utilCheckUserFile()
+        {
+            if (Directory.Exists(userAppFolder) == false)
+            { Directory.CreateDirectory(userAppFolder); }
+            //if (Directory.Exists(strFileDailyFolder) == false)
+            //{ Directory.CreateDirectory(strFileDailyFolder); }
+        }
+        public bool IsFileLocked(string filename) 
+        {
+            bool Locked = false;
+            try
+            {
+                FileStream fs =
+                    File.Open(filename, FileMode.OpenOrCreate,
+                    FileAccess.ReadWrite, FileShare.None);
+                fs.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Locked = true;
+            }
+            return Locked;
+        }
+
+        private void btnImgSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select A File";
+            openDialog.Filter = "Image Files (*.png;*.jpg;*.bmp;*.gif)|*.png;*.jpg;*.bmp;*.gif" + "|" + "All Files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openDialog.FileName;
+                txtFilePath.Text = file;
+                toolStripStatusLabel1.Text = "InputFile= " + txtFilePath.Text;
+            }
+            if (txtFilePath.Text != "") { btnToBGR.Enabled = true; }
+            else { btnToBGR.Enabled = false; }
+
+            toolStripStatusLabel1.Text = "按下 ToBGRimage 按鈕以產出圖檔";
+        }
+
+        private void btnToBGR_Click(object sender, EventArgs e)
+        {
+            // 設定輸入和輸出檔案路徑
+            string inputImagePath = txtFilePath.Text;   //"D:\\temporary\\imgs\\ITRI_img1-72x176.png"; //"input.png"; // 替換為你的輸入圖片路徑
+            string outputImagePath = userAppFolder + "\\output.png";    //"output.png"; // 替換為你的輸出圖片路徑
+            utilCheckUserFile();
+
+            try
+            {
+                // 讀取圖片
+                Bitmap inputImage = new Bitmap(inputImagePath);
+
+                // 建立輸出圖片:原圖每px展開成RGB的3pxs
+                Bitmap outputImage = new Bitmap(inputImage.Width * 3, inputImage.Height);
+
+                // 遍歷每個像素
+                for (int y = 0; y < inputImage.Height; y++)
+                {
+                    for (int x = 0; x < inputImage.Width; x++)
+                    {
+                        // 獲取像素的 RGB 值
+                        Color pixelColor = inputImage.GetPixel(x, y);
+                        byte r = pixelColor.R;
+                        byte g = pixelColor.G;
+                        byte b = pixelColor.B;
+                        Color newColor = Color.FromArgb(0, 0, 0);   //default
+                        byte Cut = 32;  //no show out if under the value
+
+                        for (int i = 0; i < 3; i++) //原圖每px展開成RGB分開的3pxs
+                        {
+                            int j = x * 3 + i;
+                            switch (i)
+                            {
+                                case 0: //b
+                                    // 建立新的顏色
+                                    if (b > Cut && (r != 0 || g != 0))
+                                    { newColor = Color.FromArgb(r, g, b); }
+                                    else
+                                    { newColor = Color.FromArgb(0, 0, b); }
+                                    break;
+                                case 1: //g
+                                    // 建立新的顏色
+                                    if (g > Cut && (r != 0 || b != 0))
+                                    { newColor = Color.FromArgb(r, g, b); }
+                                    else
+                                    { newColor = Color.FromArgb(0, g, 0); }
+                                    break;
+                                case 2: //r
+                                    // 建立新的顏色
+                                    if (r > Cut && (g != 0 || b != 0))
+                                    { newColor = Color.FromArgb(r, g, b); }
+                                    else
+                                    { newColor = Color.FromArgb(r, 0, 0); }
+                                    break;
+                                default:
+                                    Console.WriteLine("Error1");
+                                    break;
+                            }
+                            // 設定輸出圖片的像素
+                            outputImage.SetPixel(j, y, newColor);
+                        }
+                    }
+                }
+
+                // 儲存輸出圖片
+                outputImage.Save(outputImagePath, ImageFormat.Png);//或其他你想要的格式
+
+                // 釋放資源
+                inputImage.Dispose();
+                outputImage.Dispose();
+
+                Console.WriteLine("圖片處理完成，已儲存至 " + outputImagePath);
+                toolStripStatusLabel1.Text = "圖片處理完成，已儲存至 " + outputImagePath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("發生錯誤: " + ex.Message);
+                toolStripStatusLabel1.Text = "發生錯誤: " + ex.Message;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit？", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+            }
+            else { e.Cancel = true; }
+        }
+
+        protected override void WndProc(ref Message m)  //2022-11-17
+        {
+            const int WM_DEVICECHANGE = 0x219;
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_CLOSE = 0xF060;
+            const int SC_CLICK = 0xF093;
+            const int SC_DOUBLE_CLICK = 0xF063;
+            const int iCurr80mA = 146;
+
+            object ojb = new object();
+            try   //2023-09-25
+            {
+                // WM_DEVICECHANGE Message : 電腦硬體裝置改變時產生的訊息
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            Console.WriteLine("DEVICE CHANGEed");
+                            break;
+                        // DBT_DEVICEARRIVAL Event : 裝置插入並且可以使用時，產生的系統訊息
+                        case DBT_DEVICEARRIVAL:
+                            string[] portnames = SerialPort.GetPortNames();
+                            Console.WriteLine("DEVICE was inserted.");
+                            toolStripStatusLabel1.Text = "DEVICE was inserted";
+                            break;
+
+                        // DBT_DEVICEREMOVECOMPLETE Event : 裝置卸載或移除時產生的系統訊息
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            portnames = SerialPort.GetPortNames();
+                            Console.WriteLine("DEVICE was removed.");
+                            toolStripStatusLabel1.Text = "DEVICE was removed";
+                            break;
+                    }
+                }
+                else if (m.Msg == WM_SYSCOMMAND)
+                {
+                    if (m.WParam.ToInt32() == SC_CLOSE)
+                    {
+                        // 点击winform右上关闭按钮 
+                        // 加入想要的逻辑处理
+                        //utilWindowClosingWithConfirm(false);
+                        //return;
+                    }
+                    else if (m.WParam.ToInt32() == SC_CLICK)
+                    {
+                        return;
+                    }
+                    else if (m.WParam.ToInt32() == SC_DOUBLE_CLICK)
+                    {                        
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "WndProc");
+            }
+            base.WndProc(ref m);
+        }
+    }
+}
